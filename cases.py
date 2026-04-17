8fc2623014f66e186714f59a47e8b846c33f003c
Callum Boyd <cboy0015@student.monash.edu>
2026-04-18 02:32:05 +1000

    made n2o tank blowdown case

--- cases.py
+++ cases.py
@@ -1,5 +1,5 @@
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 from tanks import TankConfig, TankInitialCondition
@@ -23,11 +23,12 @@ class SimCase:
 
     # hardware config
     tank_configs: dict[str, TankConfig]
-    tank_initital_conditions: dict[str, TankInitialCondition]
-    engine_config: EngineConfig
+    tank_initial_conditions: dict[str, TankInitialCondition]
+
+    engine_config: EngineConfig | None = None
 
     # sim settings
-    settings: SimulationSettings
+    settings: SimulationSettings = field(default_factory=SimulationSettings)
 
 #------------------------------
 # defining fluids
@@ -53,7 +54,33 @@ NITROGEN = Fluid(
     cea_name=None,   # not used in combustion
 )
 
+test_injector = InjectorConfig(
+    cd=0.75,
+    area_m2=1e-5
+)
+
+n2o_tank = TankConfig(
+    name="Nitrous Oxide Tank",
+    role="oxidiser",
+    fluid=NITROUS_OXIDE,
+    tank_volume_m3=0.01,    # 10L
+    phase_model="self_pressurised",
+    injector=test_injector
+)
+
+n2o_tank_initial = TankInitialCondition(
+    mode="pressure_mass",
+    pressure_pa=6e+6,    # 60 bar
+    total_mass_kg=6.0
+)  
 
 #------------------------------
 # N2O Blowdown case
 
+n2o_blowdown_case = SimCase(
+    name="n2o_blowdown_test",
+    tank_configs={"n2o_tank": n2o_tank},
+    tank_initial_conditions={"n2o_tank": n2o_tank_initial},
+    engine_config=None,
+    settings=SimulationSettings(0.01, 20)
+)
